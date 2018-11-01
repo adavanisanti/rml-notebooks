3139f6ebbfaa7f81e7a7dcf16519f1cc4f6a70e8
Pieter M Leffers <pmleffers@hotmail.com>
2018-10-31 23:25:10 -0700

    Add files via upload

--- tensorflow_run.py
+++ tensorflow_run.py
@@ -21,15 +21,12 @@ from sklearn import metrics
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
-# For Visualization
-import matplotlib.pyplot as plt
-
 learning_rate =  0.001
 max_iter = 100
 batch_size = 100
 
-train_file = os.getcwd()+'/avazu-app.tr'
-test_file = os.getcwd()+'/avazu-app.t'
+train_file=os.getcwd()+'/avazu-app.t'
+test_file=os.getcwd()+'/avazu-app.tr'
 
 class DataSet(object):
     def __init__(self):
@@ -40,16 +37,16 @@ class DataSet(object):
         '''
         '''
         self.print_iter_counter=0
-	self.printcounter=0
+        self.printcounter=0
         self.ins_num = 0 #<set at zero
         f = open(file, "r")
         self.y = []
         self.feature_ids = []
         self.feature_values = []
         self.ins_feature_interval = []
-        self.max_ins_feature_interval = []
+        #self.max_ins_feature_interval = []
         self.ins_feature_interval.append(0)#makes zero the starting value in ins_feature_interval
-        self.max_token=[]
+        #self.max_token=[]
         for line in f.readlines():#iterating through open file
             regexp = re.compile(r':')#<---If feature has a colon then do
             tokens = line.split(" ")#split lines in the file
@@ -69,17 +66,22 @@ class DataSet(object):
             for feature in tokens:#(len(tokens)~16
                 self.printcounter+= 1
                 self.print_iter_counter+=1
-                if (self.printcounter==100000):
+                if (self.printcounter==200000):
                     print(feature,'Count:',self.print_iter_counter)
                     self.printcounter=0
                 if regexp.search(feature):#if there is a colon in feature
-                    self.max_token.append(feature)#check on size
+                    #self.max_token.append(feature)#check on size
                     feature_id, feature_value = feature.split(":") #split on colon
                     if feature_id:
                         self.feature_ids.append(int(feature_id))#append to feature ids
                         self.feature_values.append(float(feature_value)) #append feature values
             self.ins_num += 1 #set ins_num to 1
-	self.print_iter_counter=0
+        #housecleaning
+        tokens=''
+        self.print_iter_counter=0
+        self.printcounter=0
+        f=()
+        
         self.feature_num=max(self.feature_ids)#modify feature_num to max of ids (maximum # of features)
         #self.max_ins_feature_interval=max(self.ins_feature_interval)
         print('the max number of features:',self.feature_num)
@@ -100,7 +102,7 @@ class DataSet(object):
             #(begin, end)setting bounds moving across batch length in data
             #print((begin, end))#slicing action of data (0, 15) (15, 30) (30, 45) (60,...
         return self.slice(begin, end)
-#Error
+
     def slice(self, begin, end):
             sparse_index = []
             sparse_ids = []
@@ -136,28 +138,26 @@ class DataSet(object):
             y = np.array(self.y[begin:end]).reshape((end - begin, 1))
             #begin:0,30,60,90,120,150,180,210 intervals of 30
             #end: 30,60,90,120,150,180,210,240
+   
             #            0            0                       0                 30            30
             #print(len(sparse_index), len(sparse_ids), len(sparse_values), len(sparse_shape), len(y))
             return (sparse_index, sparse_ids, sparse_values, sparse_shape, y)
 
 class BinaryLogisticRegression(object):
     def __init__(self, feature_num):
-        self.feature_num = feature_num
-        self.sparse_index = tf.placeholder(tf.int64)
-        self.sparse_ids = tf.placeholder(tf.int64)
-        self.sparse_values = tf.placeholder(tf.float32)
-        self.sparse_shape = tf.placeholder(tf.int64)
-        self.w = tf.Variable(tf.random_normal([self.feature_num, 1], stddev=0.1))
-        self.y = tf.placeholder("float", [None, 1])
+    self.feature_num = feature_num
+    self.sparse_index = tf.placeholder(tf.int64)
+    self.sparse_ids = tf.placeholder(tf.int64)
+    self.sparse_values = tf.placeholder(tf.float32)
+    self.sparse_shape = tf.placeholder(tf.int64)
+    self.w = tf.Variable(tf.random_normal([self.feature_num, 1], stddev=0.1))
+    self.y = tf.placeholder("float", [None, 1])
 
     def forward(self):
-        return tf.nn.embedding_lookup_sparse(self.w,
-                                             tf.SparseTensor(self.sparse_index, self.sparse_ids, self.sparse_shape),
-                                             tf.SparseTensor(self.sparse_index, self.sparse_values, self.sparse_shape),
-                                             combiner="sum")
+    return tf.nn.embedding_lookup_sparse(self.w,tf.SparseTensor(self.sparse_index, self.sparse_ids, self.sparse_shape),tf.SparseTensor(self.sparse_index, self.sparse_values, self.sparse_shape),combiner="sum")
 
-mem_baseline=psutil.virtual_memory() #  physical memory usage
-print('Here is the memory baseline prior to importing data:\n\n',mem_baseline)
+mem_baseline1=psutil.virtual_memory() #  physical memory usage
+print('Here is the memory baseline prior to importing data:\n\n',mem_baseline1)
 
 start = time.time()
 
@@ -165,21 +165,21 @@ train_set = DataSet()
 train_set.load(train_file)
 
 end = time.time()
-exec_time=(end - start)
-print('\nThe time taken to import and prepare training data for Tensorflow is:',exec_time)
+exec_time1=(end - start)
+print('\nThe time taken to import and prepare training data for Tensorflow is:',exec_time1)
 
 start = time.time()
 
 test_set = DataSet()
 test_set.load(test_file)
-
+feature_num=test_set.feature_num
 
 end = time.time()
-exec_time=(end - start)
-print('\nThe time taken to import and prepare testing data for Tensorflow is:',exec_time)
+exec_time2=(end - start)
+print('\nThe time taken to import and prepare testing data for Tensorflow is:',exec_time2)
 
-mem_baseline=psutil.virtual_memory() #  physical memory usage
-print('Here is the memory usage after importing data:\n',mem_baseline)
+mem_baseline2=psutil.virtual_memory() #  physical memory usage
+print('Here is the memory usage after importing data:\n',mem_baseline2)
 
 model = BinaryLogisticRegression(feature_num)
 
@@ -196,13 +196,13 @@ init_all_variable = tf.global_variables_initializer()
 init_local_variable = tf.local_variables_initializer()
 session.run([init_all_variable, init_local_variable])
 
-num_passes=5 #number of passes in for loop
-
+num_passes=1
 start = time.time()
 end_list=[]
 for i in range(0,num_passes):
     while train_set.epoch_pass < max_iter:
         sparse_index, sparse_ids, sparse_values, sparse_shape, mb_y = train_set.mini_batch(batch_size)
+        
         _, loss_, prob_out = session.run([optimizer, loss, probability_output],
                                          feed_dict={model.sparse_index: sparse_index,
                                                     model.sparse_ids: sparse_ids,
@@ -213,7 +213,7 @@ for i in range(0,num_passes):
     end = time.time()
     exec_time=(end - start)
     end_list.append(exec_time) 
-
+    #save endlist not exec_time
     try:
         auc = roc_auc_score(mb_y, prob_out)
         print("epoch: ", train_set.epoch_pass, " ROC AUC score is: ", auc)
@@ -225,4 +225,7 @@ for i in range(0,num_passes):
 
 print('\nThe average time taken to execute logistic regression for '+str(num_passes)+' full passes of',max_iter,'iterations took',np.array(end_list).mean(),'seconds with a standard deviation of +- '+str(np.array(end_list).std()))
 
-
+bench_list=[str(mem_baseline1),str(exec_time1),str(exec_time2),str(mem_baseline2),str(np.array(end_list).mean())]
+with open('bench_times.txt', 'w') as f:
+    for item in bench_list:
+        f.write("%s\n" % item)
